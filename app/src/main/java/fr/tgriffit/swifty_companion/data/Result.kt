1d228e2c275a16ec33a232d6dc71727a869f89ab
sansho88 <sansho88@hotmail.fr>
2024-03-20 15:46:19 +0100

    Save before 42 Lyon sessions reset

---
+++ app/src/main/java/fr/tgriffit/swifty_companion/data/Result.kt
@@ -0,0 +1,18 @@
+package fr.tgriffit.swifty_companion.data
+
+/**
+ * A generic class that holds a value with its loading status.
+ * @param <T>
+ */
+sealed class Result<out T : Any> {
+
+    data class Success<out T : Any>(val data: T) : Result<T>()
+    data class Error(val exception: Exception) : Result<Nothing>()
+
+    override fun toString(): String {
+        return when (this) {
+            is Success<*> -> "Success[data=$data]"
+            is Error -> "Error[exception=$exception]"
+        }
+    }
+}
---
+++ app/src/main/java/fr/tgriffit/swifty_companion/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package fr.tgriffit.swifty_companion.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+    val userId: String,
+    val displayName: String
+)
