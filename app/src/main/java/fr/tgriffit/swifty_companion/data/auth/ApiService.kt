2adb8468254a563412405c6bb79b04820780886e
tgriffit <sansho88@hotmail.fr>
2024-05-14 16:29:58 +0200

    Recuperation de la page de demande d'acces a l'intra

--- app/src/main/java/fr/tgriffit/swifty_companion/data/auth/ApiService.kt
+++ app/src/main/java/fr/tgriffit/swifty_companion/data/auth/ApiService.kt
@@ -2,21 +2,16 @@ package fr.tgriffit.swifty_companion.data.auth
 
 import android.content.ContentValues
 import android.content.ContentValues.TAG
-import android.os.AsyncTask
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
-import com.github.kittinunf.fuel.Fuel
-import com.github.kittinunf.fuel.core.extensions.authentication
 import com.github.kittinunf.fuel.httpGet
 import com.github.kittinunf.fuel.httpPost
 import com.github.kittinunf.result.Result
 import com.google.gson.Gson
 import io.github.cdimascio.dotenv.dotenv
-import java.io.BufferedOutputStream
 import java.lang.Exception
-import java.net.HttpURLConnection
-import java.net.URL
+import java.util.UUID
 import java.util.concurrent.Executors
 
 
@@ -25,13 +20,18 @@ import java.util.concurrent.Executors
  * https://api.intra.42.fr/apidoc/guides/getting_started
  */
 class ApiService {
-    private val dotenv = dotenv{
+    private val dotenv = dotenv{ //path to .env file: app/src/main/assets/env
         directory = "/assets"
         filename = "env" // instead of '.env', use 'env'
         ignoreIfMissing = true
     }
     private val clientId = dotenv["UID"] //add your client id
     private val clientSecret = dotenv["SECRET"] //add your client secret
+    private val redirectUri = "swifty://callback" //add your redirect uri ( https://www.oauth.com/oauth2-servers/redirect-uris/redirect-uris-native-apps/ )
+    private val scope = "public"
+    private var state = "12345"
+    private val responseType = "code"
+    private val getAccess42ApiUrl = "https://api.intra.42.fr/oauth/authorize"
     private val apigeeTokenUrl = "https://api.intra.42.fr/oauth/token" // url to get the token
     private val grantType = "client_credentials"
 
@@ -59,7 +59,7 @@ class ApiService {
             }
 
     }
-    private fun setAuthToken() {
+    private fun setPublicAuthToken() {
 
             try {
                 val credentials = mapOf<String, String>(
@@ -78,7 +78,8 @@ class ApiService {
                     is Result.Success -> {
                         var gson = Gson()
                         val tokenResultJson = gson.fromJson(result.value, AuthResult::class.java)
-                        Log.d(TAG, "setAuthToken: RESULT VALUUUUE:\n${result.value}")
+                        Log.d(TAG, "[SUCCESS] setAuthToken:\n" +
+                                "RESULT VALUUUUE:\n${result.value}")
                         token = tokenResultJson.access_token
                         tokenType = tokenResultJson.token_type
                         Log.d(TAG, "token $token")
@@ -86,8 +87,14 @@ class ApiService {
                     }
 
                     is Result.Failure -> {
-                        // handle error
-                        Log.e(TAG, "setAuthToken: FAILED:\n" +
+                        if (response.statusCode == 401)
+                            Log.e(TAG, "=======================\n" +
+                                    "setAuthToken: 401 Unauthorized\n" +
+                                    "Check if the client_secret in the env file is still up-to-date" +
+                                    "\n(app/src/main/assets/env)" +
+                                    "\n=======================")
+                        else
+                            Log.e(TAG, "setAuthToken: FAILED:\n" +
                                 "UID=$clientId\n" +
                                 "SECRET=$clientSecret\n" +
                                 "request => $request" +
@@ -101,9 +108,57 @@ class ApiService {
 
     }
 
+    private fun request42AccessToUser(){
+
+        val authorizationUrl = "https://api.intra.42.fr/oauth/authorize?" +
+                "client_id=$clientId&" +
+                "redirect_uri=$redirectUri&" +
+                "scope=$scope&" +
+                "response_type=code"
+        try {
+            state = UUID.randomUUID().toString()
+
+            val (request, response, result) = getAccess42ApiUrl.httpGet(listOf(
+                "client_id" to clientId,
+                "redirect_uri" to redirectUri,
+                "scope" to scope,
+                "state" to state,
+                "response_type" to responseType
+            ))
+                .responseString()
+
+            when (result) {
+                is Result.Success -> {
+                   Log.d(TAG, "[SUCCESS] request42AccessToUser:\n" +
+                           "Ask for access page:${result.value}")
+                }
+
+                is Result.Failure -> {
+                    if (response.statusCode == 401)
+                        Log.e(TAG, "=======================\n" +
+                                "request42AccessToUser: 401 Unauthorized\n" +
+                                "Check if the client_secret in the env file is still up-to-date" +
+                                "\n(app/src/main/assets/env)" +
+                                "\n=======================")
+                    else
+                        Log.e(TAG, "request42AccessToUser: FAILED:\n" +
+                                "UID=$clientId\n" +
+                                "SECRET=$clientSecret\n" +
+                                "request => $request" +
+                                "\n Response => $response", )
+                }
+            }
+
+        }catch (e: Exception){
+            e.printStackTrace()
+        }
+    }
+
+
     init {
         executor.execute {
-            setAuthToken()
+            setPublicAuthToken()
+            request42AccessToUser()
             callApi("https://api.intra.42.fr/v2/users", tokenType, token)
             //callApi("https://api.intra.42.fr/v2/me", tokenType, token) //todo: oauth42 pour pouvoir la faire! (https://api.intra.42.fr/apidoc/guides/web_application_flow)
         }
